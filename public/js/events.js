e76a0183a0b72f410fb18fa1e545eed83ed67b48
Chad Barraford <chad@cloudant.com>
2013-09-11 17:52:15 -0400

    minor improvements with collapsing events page sections

--- public/js/events.js
+++ public/js/events.js
@@ -1,19 +1,39 @@
 $(document).ready(function(){
   $("#collapseStats").click(function(){
-    $("#stats_field").toggle();
+    if ($("#stats_field").is(":visible")) {
+      $("#stats_field").hide();
+      $(this).children("span").removeClass("glyphicon-chevron-up");
+      $(this).children("span").addClass("glyphicon-chevron-down");
+    } else {
+      $("#stats_field").show();
+      $(this).children("span").removeClass("glyphicon-chevron-down");
+      $(this).children("span").addClass("glyphicon-chevron-up");
+    };
   });
   $("#collapseFilters").click(function(){
-    $("#filters_field").toggle();
+    if ($("#filters_field").is(":visible")) {
+      $("#filters_field").hide();
+      $(this).children("span").removeClass("glyphicon-chevron-up");
+      $(this).children("span").addClass("glyphicon-chevron-down");
+    } else {
+      $("#filters_field").show();
+      $(this).children("span").removeClass("glyphicon-chevron-down");
+      $(this).children("span").addClass("glyphicon-chevron-up");
+    };
   });
   $("#collapseBrowser").click(function(){
     if ($("#browse_list").is(":visible")) {
       $("#browse_list").hide();
       $(".main-content").removeClass("col-md-9");
       $(".main-content").addClass("col-md-12");
+      $(this).children("span").removeClass("glyphicon-chevron-down");
+      $(this).children("span").addClass("glyphicon-chevron-up");
     } else {
       $("#browse_list").show();
       $(".main-content").removeClass("col-md-12");
       $(".main-content").addClass("col-md-9");
+      $(this).children("span").removeClass("glyphicon-chevron-up");
+      $(this).children("span").addClass("glyphicon-chevron-down");
     };
   });
 });
