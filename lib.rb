adf7359a597498509a873a021517e3aaad4d298a
Chad Barraford <chad@cloudant.com>
2013-09-11 14:48:40 -0400

    removed old method no longer need

--- lib.rb
+++ lib.rb
@@ -39,14 +39,4 @@ helpers do
     end
   end
 
-  def authorized?
-    return true if is_logged_in? == true
-    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
-    if @auth.provided? && @auth.basic? && @auth.credentials && validate(@auth.credentials[0], @auth.credentials[1])
-      session[:username] = @auth.username
-    else
-      return false
-    end
-  end
-
 end
