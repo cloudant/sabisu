4515ac02a44299356e3dac375d09e894f8db7518
Chad Barraford <chad@cloudant.com>
2013-09-11 19:02:24 -0400

    fixed browser button to be in the right place

--- public/js/events.js
+++ public/js/events.js
@@ -26,12 +26,16 @@ $(document).ready(function(){
       $("#browse_list").hide();
       $(".main-content").removeClass("col-md-9");
       $(".main-content").addClass("col-md-12");
+      $(this).removeClass("collapseBrowserVisable");
+      $(this).addClass("collapseBrowserNonVisable");
       $(this).children("span").removeClass("glyphicon-chevron-down");
       $(this).children("span").addClass("glyphicon-chevron-up");
     } else {
       $("#browse_list").show();
       $(".main-content").removeClass("col-md-12");
       $(".main-content").addClass("col-md-9");
+      $(this).removeClass("collapseBrowserNonVisable");
+      $(this).addClass("collapseBrowserVisable");
       $(this).children("span").removeClass("glyphicon-chevron-up");
       $(this).children("span").addClass("glyphicon-chevron-down");
     };
