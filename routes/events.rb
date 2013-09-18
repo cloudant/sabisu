ffd0ce5c34d350481288936bb5da868b1ee59c34
Chad Barraford <chad@cloudant.com>
2013-09-18 11:11:12 -0400

    don't pass events to template

--- routes/events.rb
+++ routes/events.rb
@@ -1,6 +1,5 @@
 get '/events' do
-  events = CURRENT_DB.view('sort/client', :skip => 5, :limit => 10, :descending => true, :include_docs => true)['rows']
-  haml :events, :locals => { :events => events }
+  haml :events
 end
 
 get '/events/:client' do
