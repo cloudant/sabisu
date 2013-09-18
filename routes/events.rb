db097bc9177f7018c4ed2445e0e1b07812e9b4bb
Chad Barraford <chad@cloudant.com>
2013-09-18 15:23:23 -0400

    clients get its own route

--- routes/events.rb
+++ routes/events.rb
@@ -1,11 +1,3 @@
 get '/events' do
   haml :events
 end
-
-get '/events/:client' do
-  haml :events_client, :locals => { :client => params[:client] }
-end
-
-get '/events/:client/:check' do
-  haml :events_client_check, :locals => { :client => params[:client], :check => params[:check] }
-end
--- routes/init.rb
+++ routes/init.rb
@@ -1,3 +1,4 @@
 require_relative 'events'
 require_relative 'api'
 require_relative 'sensu'
+require_relative 'client'
