5d1e5adc6a16e3fd3cec0c2cd067c8206048b6ad
Chad Barraford <cbarraford@gmail.com>
2014-01-12 17:09:18 -0500

    add events routes

--- routes/events.rb
+++ routes/events.rb
@@ -1,3 +1,19 @@
 get '/events' do
   haml :events
 end
+
+get '/api/events' do
+  params = request.env['rack.request.query_hash']
+  events = Event.all(params)
+  puts events
+  events[:events].each do |event|
+    unless event.nil?
+      puts event
+    end
+  end
+end
+
+
+get '/api/events/search' do
+  params = request.env['rack.request.query_hash']
+end
